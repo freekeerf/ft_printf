6cbc863af4880735aa6652f3851b7e335cea012a
Evgeniy <mobil@freek.kiev.ua>
2018-10-15 15:54:24 +0300

    dfs

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -33,7 +33,7 @@ int		ft_printf(char *format, ...)
 	va_list			args;
 	int				i;
 
-//ß	printf("format = %s\n!", format);
+	printf("format = %s\n!", format);
 //	while ()
 //	printf("arg = %d\n!", va_arg(args, int));
 	flags = ft_memalloc(sizeof(t_flags));
